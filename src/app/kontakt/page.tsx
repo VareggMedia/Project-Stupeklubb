5ba40dde19afaaa6698246acf56de163fe24059a
Anand <119463874+Tjommiboy@users.noreply.github.com>
2026-09-22 11:57:29 +0200

    Reorganize layout (#22)
    * lille test
    
    * forenklet hjemmeside
    
    * mindre detaljer
    
    * organiserer nav links og layout
    
    * connected pages
    
    * Add flag language switcher to navbar
    
    * reorganize navbar links and update page links for consistency
    
    * added new page links
    
    ---------
    
    Co-authored-by: inferixon <alekzmay@gmail.com>

---
+++ src/app/kontakt/page.tsx
@@ -0,0 +1,9 @@
+export default function kontakt() {
+  return (
+    <main>
+      <h1>kontakt</h1>
+
+      <p>Get in touch with us.</p>
+    </main>
+  );
+}
---
+++ src/app/pamelding/page.tsx
@@ -0,0 +1,11 @@
+export default function pamelding() {
+  return (
+    <div className="bsk pamelding">
+      <main>
+        <h1>påmelding</h1>
+
+        <p>Get in touch with us.</p>
+      </main>
+    </div>
+  );
+}
